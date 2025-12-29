1a90e2a7969d9989b9ea97a4e42a643418818320
acepilot950-creator <acepilot950@gmail.com>
2025-12-30 00:26:48 +0400

    initial commit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Checks if an object is an instance of a class inherited from another class."""
+"""
+Module that provides a function to check if an object is an instance
+of a class that inherited (directly or indirectly) from another class.
+"""
 
 
 def inherits_from(obj, a_class):
     """
     Return True if obj is an instance of a class that inherited
-    (directly or indirectly) from a_cl_
+    (directly or indirectly) from a_class, but is not an instance
+    of a_class itself.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
