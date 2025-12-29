ced47f9770e42fbdb4da3601f18aacedd19c110d
acepilot950-creator <acepilot950@gmail.com>
2025-12-30 00:33:15 +0400

    initial commit

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class with validation methods.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class with area method and integer validation.
+    """
+
+    def area(self):
+        """
+        Raises an Exception indicating that the area
