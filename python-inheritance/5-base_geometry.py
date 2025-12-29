a7184d0ca5d35b591d15a8589fe71d626a85f44c
acepilot950-creator <acepilot950@gmail.com>
2025-12-30 00:29:18 +0400

    initial commit

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class BaseGeometry used as a base for geometry objects.
+    """
+    pass
