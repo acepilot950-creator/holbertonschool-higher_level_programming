a1a5afa629af38909e34786b4de7654e198fc51d
acepilot950-creator <acepilot950@gmail.com>
2025-12-29 17:52:41 +0400

    initial commit

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -38,4 +38,34 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the rectangle drawn with the # character."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        lines = []
+        for _ in range(self.__height):
+            lines.append("#" * self.__width)
+        return "\n".join(lines)
+
+    def __repr__(self):
+        """Return a string representation to recreate the instance."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print a message and decrement instance counter on deletion."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
