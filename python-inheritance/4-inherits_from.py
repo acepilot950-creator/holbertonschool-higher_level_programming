1e2e9a0a8b14434e35490878ad0cfd1bf35b1068
acepilot950-creator <acepilot950@gmail.com>
2025-12-30 00:25:30 +0400

    initial commit

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Checks if an object is an instance of a class inherited from another class."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True if obj is an instance of a class that inherited
+    (directly or indirectly) from a_cl_
