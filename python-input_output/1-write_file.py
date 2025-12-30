867002b833078fc6950482b710af2a461364b418
acepilot950-creator <acepilot950@gmail.com>
2025-12-30 15:29:11 +0400

    initial commit

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Writes a string to a text file (UTF8) and returns the number of characters written."""
+
+
+def write_file(filename="", text=""):
+    """Write text to a file and return the number of characters written."""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
