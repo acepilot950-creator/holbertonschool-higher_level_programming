28981037222dcab7fddb189ee5771a2d1579595c
acepilot950-creator <acepilot950@gmail.com>
2025-12-30 15:24:51 +0400

    initial commit

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reads a text file (UTF8) and prints it to stdout."""
+
+
+def read_file(filename=""):
+    """Read a text file and print its content to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
