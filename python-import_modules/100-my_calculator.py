737cd35f1910a48bec1cccb20fd7774999b7fd56
acepilot950-creator <acepilot950@gmail.com>
2026-01-12 14:53:01 +0400

    initial commit

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import calculator_1
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+
+    if operator == "+":
+
+        result = calculator_1.add(a, b)
+
+    elif operator == "-":
+        result = calculator_1.sub(a, b)
+
+    elif operator == "*":
+        result = calculator_1.mul(a, b)
+
+    elif operator == "/":
+
+        result = calculator_1.div(a, b)
+
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print(a, operator, b, "=", result)
